ddee312848649e9e8937dfadd0e2c1bcbc0b8af1
Luc van den Brand <10974297+LucvandenBrand@users.noreply.github.com>
2019-01-09 15:40:00 +0100

    Made default audio play.

--- tools/MIDIPlayer/src/audio/audio.h
+++ tools/MIDIPlayer/src/audio/audio.h
@@ -2,7 +2,25 @@
 #define AUDIO_H
 
 typedef struct {
-    int numNotes;
+    unsigned char note;
+    unsigned char duration;
+} AudioEvent;
+
+typedef struct {
+    AudioEvent * channel1;
 } Audio;
+
+AudioEvent defaultChannel1[6] = {
+    {0x02, 1},
+    {0x05, 1},
+    {0x12, 4},
+    {0x02, 1},
+    {0x05, 1},
+    {0x12, 4}
+};
+
+Audio defaultAudio = {
+    defaultChannel1
+};
 
 #endif
--- tools/MIDIPlayer/src/main.c
+++ tools/MIDIPlayer/src/main.c
@@ -4,17 +4,17 @@
 
 void initAudioSystem() {
     // turn sound on
-    REG_SNDSTAT= SSTAT_ENABLE;
+    REG_SNDSTAT = SSTAT_ENABLE;
     // snd1 on left/right ; both full volume
     REG_SNDDMGCNT = SDMG_BUILD_LR(SDMG_SQR1, 7);
     // DMG ratio to 100%
-    REG_SNDDSCNT= SDS_DMG100;
+    REG_SNDDSCNT = SDS_DMG100;
 
     // no sweep
-    REG_SND1SWEEP= SSW_OFF;
+    REG_SND1SWEEP = SSW_OFF;
     // envelope: vol=12, decay, max step time (7) ; 50% duty
-    REG_SND1CNT= SSQR_ENV_BUILD(12, 0, 7) | SSQR_DUTY1_2;
-    REG_SND1FREQ= 0;
+    REG_SND1CNT = SSQR_ENV_BUILD(12, 0, 7) | SSQR_DUTY1_2;
+    REG_SND1FREQ = 0;
 }
 
 void playNoteAtOctave(int note, int octave) {
@@ -22,7 +22,12 @@ void playNoteAtOctave(int note, int octave) {
 }
 
 void playAudio() {
-    playNoteAtOctave(0x02&15, 0x02>>4);
+    for (int i=0; i<6; i++) {
+        unsigned char note = defaultAudio.channel1[i].note;
+        playNoteAtOctave(note&15, note>>4);
+        unsigned char delay = defaultAudio.channel1[i].duration;
+        VBlankIntrDelay(8*delay);
+    }
 }
 
 int main() {
